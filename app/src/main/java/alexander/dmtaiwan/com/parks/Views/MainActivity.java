531ef1c1dd0e783305b1b6d50280fffbb990cc71
lenovo <ajhollid@gmail.com>
2015-11-25 20:59:56 +0800

    Implement navigation drawer

--- app/src/main/java/alexander/dmtaiwan/com/parks/Views/MainActivity.java
+++ app/src/main/java/alexander/dmtaiwan/com/parks/Views/MainActivity.java
@@ -2,25 +2,42 @@ package alexander.dmtaiwan.com.parks.Views;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import alexander.dmtaiwan.com.parks.Presenters.MainPresenter;
 import alexander.dmtaiwan.com.parks.Presenters.MainPresenterImpl;
 import alexander.dmtaiwan.com.parks.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements MainView {
+public class MainActivity extends AppCompatActivity implements MainView, NavigationView.OnNavigationItemSelectedListener {
 
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @Bind(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
     private MainPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,9 +50,32 @@ public class MainActivity extends AppCompatActivity implements MainView {
             }
         });
 
+        setupToolbar();
+        setupNavDrawer();
+
         mPresenter = new MainPresenterImpl(this, this);
         if (mPresenter != null) {
             mPresenter.requestData();
+        }
+    }
+
+    private void setupNavDrawer() {
+        if (mToolbar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(android.R.drawable.ic_media_play);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    private void setupToolbar() {
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
         }
     }
 
@@ -64,5 +104,20 @@ public class MainActivity extends AppCompatActivity implements MainView {
     @Override
     public void onDataReturned() {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+        switch (item.getItemId()) {
+            case R.id.drawer_home:
+                Log.i(LOG_TAG, "Home");
+                return true;
+            case R.id.drawer_map:
+                Log.i(LOG_TAG, "Map");
+                return true;
+            default:
+                return true;
+        }
     }
 }
